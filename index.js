e1c1363f22c6b15f0a380879245972a02ddfeae0
Naresh Oli <olinaresh85@gmail.com>
2023-03-15 11:30:57 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Naresh';
+
+const welcomeMessage = "Welcome to Flatbook, Naresh!"
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, NARESH!"
+
+const shortGreeting = "Welcome, N!"
